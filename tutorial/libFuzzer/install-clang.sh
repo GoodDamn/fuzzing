56ce26b340149e2f5779924f536218377bc412b8
GoodDamn <grigorydum@gmail.com>
2026-05-13 12:34:48 +0300

    delete tar

--- tutorial/libFuzzer/install-clang.sh
+++ tutorial/libFuzzer/install-clang.sh
@@ -2,8 +2,12 @@
 CLANG_VERSION=17.0.4
 UBUNTU_VERSION=22.04
 CLANG_DIR=clang+llvm-$CLANG_VERSION-x86_64-linux-gnu-ubuntu-$UBUNTU_VERSION
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${CLANG_VERSION}/${CLANG_DIR}.tar.xz
-sudo tar -xf ${CLANG_DIR}.tar.xz
+CLANG_TAR=${CLANG_DIR}.tar.xz
+
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-${CLANG_VERSION}/
+$CLANG_TAR
+tar -xf $CLANG_TAR
+rm $CLANG_TAR
 sudo rm -rf /usr/local/bin/clang* /usr/local/lib/clang
 sudo cp -rf  $CLANG_DIR/bin/*  /usr/local/bin
 sudo cp -rf  $CLANG_DIR/lib/clang  /usr/local/lib
